7714fcf98fb88acfa48f36e416db2507671ca6d6
IhorPohiliy <yapazhiloyigogosha@gmail.com>
2023-10-03 20:21:01 +0300

    ver2

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,7 @@
 import { isMobile } from "./functions.js";
 // Підключення списку активних модулів
 import { flsModules } from "./modules.js";
+
+window.addEventListener('scroll', function () {
+	scrollY > 0 ? document.querySelector('.header').classList.add('scroll') : document.querySelector('.header').classList.remove('scroll')
+})
